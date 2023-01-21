46e473fb44473740910ddb6546b710d47f36f11b
Yuriy <example@gmail.com>
2023-01-21 11:22:17 +0200

    first commit

---
+++ JS/scripts.js
@@ -0,0 +1,63 @@
+"use strict";
+/* let number = 5;
+const Lelf = 1;
+
+number = 10;
+console.log(number); */
+/* let arr = ["plun.png", "orange", 6, "apple.bmp", {}, []];
+console.log(arr[3]); */
+/* const arr = ["a", "b", "c"];
+const arrObj = {
+    0: "a",
+    1: "b",
+    2: "c",
+    abc: {
+        def: {},
+    },
+};
+
+arrObj.b = "1234";
+arrObj["bc"] = "12345";
+arrObj.ab = "pizdaBlya"; */
+/* console.log(arrObj["b"]);
+console.log(arrObj.b);
+console.log(arrObj.bc);
+console.log(arrObj["ab"]);
+console.log(arrObj["0"]); */
+/* console.log(arrObj);
+
+let storeName = "PiratBay";
+let storeDescription = {
+    budget: 10000,
+    employees: ["Yuri", "Roman", "Marija"],
+    products: {
+        apple: 3,
+        banana: 5,
+    },
+    open: true,
+};
+console.log(storeDescription);
+ */
+/* const result = confirm("Are you here?");
+console.log(result);
+ */
+/* const answer = prompt("Вам есть 18?", "18");
+console.log(answer + 5);
+ */
+/* const answers = [];
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваша фамилия?", "");
+answers[2] = prompt("Сколько Вам лет?", "");
+document.write(answers); */
+/* const category = "toys";
+console.log(`https://someurl.com/ ${category}/5`);
+const user = "Ivan";
+alert(`hello, ${user}`); */
+/* console.log("arr" + " - object");
+console.log(4 + +"5"); */
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
